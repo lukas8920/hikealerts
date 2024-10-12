62859f4be9aa8afd462a0340a5f3a066f6c2eb78
lukas8920 <42778010+lukas8920@users.noreply.github.com>
2024-10-13 12:29:40 +1300

    generalised insert_geodata_trails.sql for multiple countries

--- database/procedures/insert_geodata_trails.sql
+++ database/procedures/insert_geodata_trails.sql
@@ -1,5 +1,6 @@
 CREATE PROCEDURE InsertGeodataTrails
-	@id BIGINT,
+	@trail_id BIGINT,
+	@country VARCHAR(2),
 	@trailname NVARCHAR(MAX),
 	@maplabel NVARCHAR(MAX),
 	@unitcode NVARCHAR(MAX),
@@ -9,13 +10,11 @@ CREATE PROCEDURE InsertGeodataTrails
     @lineString NVARCHAR(MAX)
 AS
 BEGIN
-	SET IDENTITY_INSERT us_geodata_trails ON;
-	MERGE INTO dbo.us_geodata_trails AS target
-	USING (SELECT @id as id, @trailname as trailname, @maplabel as maplabel, @unitcode as unitcode, @unitname as unitname, @regioncode as regioncode, @maintainer as maintainer, geometry::STGeomFromText(@lineString, 4326) AS coordinates) AS source
-	ON target.id = source.id
+	MERGE INTO dbo.geodata_trails AS target
+	USING (SELECT @trail_id as trail_id, @country as country, @trailname as trailname, @maplabel as maplabel, @unitcode as unitcode, @unitname as unitname, @regioncode as regioncode, @maintainer as maintainer, geometry::STGeomFromText(@lineString, 4326) AS coordinates) AS source
+	ON target.trail_id = source.trail_id AND target.country = source.country 
 	WHEN MATCHED THEN
-    	UPDATE SET target.trailname = source.trailname, target.maplabel = source.maplabel, target.unitcode = source.unitcode, target.unitname = source.unitname, target.regioncode = source.regioncode, target.maintainer = source.maintainer, target.coordinates = source.coordinates
+    	UPDATE SET target.country = source.country, target.trailname = source.trailname, target.maplabel = source.maplabel, target.unitcode = source.unitcode, target.unitname = source.unitname, target.regioncode = source.regioncode, target.maintainer = source.maintainer, target.coordinates = source.coordinates
 	WHEN NOT MATCHED THEN
-    	INSERT (id, trailname, maplabel, unitcode, unitname, regioncode, maintainer, coordinates) VALUES (source.id, source.trailname, source.maplabel, source.unitcode, source.unitname, source.regioncode, source.maintainer, source.coordinates);
-   	SET IDENTITY_INSERT us_geodata_trails OFF;
+    	INSERT (trail_id, country, trailname, maplabel, unitcode, unitname, regioncode, maintainer, coordinates) VALUES (source.trail_id, source.country, source.trailname, source.maplabel, source.unitcode, source.unitname, source.regioncode, source.maintainer, source.coordinates);
 END
