3e3a0295adfe97c0397075c3f7b9ee0733e11d1e
Lukas <lukas.kehrbusch@live.de>
2024-10-20 17:55:03 +1300

    added geojson angular connection

--- spring-boot/src/main/java/org/devbros/microsoft_hackathon/map_layer/MapLayerController.java
+++ spring-boot/src/main/java/org/devbros/microsoft_hackathon/map_layer/MapLayerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/v1/map")
 public class MapLayerController {
     private final MapLayerService mapLayerService;
 
@@ -17,7 +17,7 @@ public class MapLayerController {
         this.mapLayerService = mapLayerService;
     }
 
-    @GetMapping
+    @GetMapping("/layer")
     public ResponseEntity<Resource> getJsonLayer() {
         Resource resource = this.mapLayerService.loadJsonLayer();
 
