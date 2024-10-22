5d5f33398673c7bf9444202b1aa6bfd25c5e7dff
Lukas <lukas.kehrbusch@live.de>
2024-10-22 15:02:04 +1300

    added gpg webservice call

--- spring-boot/src/main/java/org/devbros/microsoft_hackathon/security/KeyVaultProdProvider.java
+++ spring-boot/src/main/java/org/devbros/microsoft_hackathon/security/KeyVaultProdProvider.java
@@ -2,24 +2,29 @@ package org.devbros.microsoft_hackathon.security;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import retrofit2.Response;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
 @Profile("prod")
 public class KeyVaultProdProvider {
+    private static final Logger logger = LoggerFactory.getLogger(KeyVaultProdProvider.class.getName());
+
     private static final String eventEndpoint = "event-endpoint-bearer-token";
     private static final String dbUrl = "sql-server-jdbc-string";
     private static final String sqlPassword = "sql-server-password";
@@ -35,10 +40,29 @@ public class KeyVaultProdProvider {
     private String keystoreLocation;
 
     @Bean
-    public SecretClient createSecretClient(CredentialService credentialService) {
-        String tenantId = credentialService.getPassword("keyvault/tenantid");
-        String clientId = credentialService.getPassword("keyvault/clientíd");
-        String password = credentialService.getPassword("keyvault/password");
+    public SecretClient createSecretClient(GpgService gpgService) throws IOException {
+        String tenantId = null;
+        String clientId = null;
+        String password = null;
+
+        try {
+            Response<GpgSecret> tenantIdResponse = gpgService.getSecret("keyvault/tenantid").execute();
+            Response<GpgSecret> clientIdResponse = gpgService.getSecret("keyvault/clientid").execute();
+            Response<GpgSecret> passwordResponse = gpgService.getSecret("keyvault/password").execute();
+
+            if (tenantIdResponse.body() != null && clientIdResponse.body() != null && passwordResponse.body() != null) {
+                tenantId = tenantIdResponse.body().getPassword();
+                clientId = clientIdResponse.body().getPassword();
+                password = passwordResponse.body().getPassword();
+            }
+        } catch (IOException e) {
+            logger.error("Error while requesting secrets from GPG Server: " + e.getMessage());
+            throw e;
+        }
+
+        if(tenantId != null && clientId != null && password != null){
+            logger.info("Successfully requested keys from gpg.");
+        }
 
         ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
                 .tenantId(tenantId)
