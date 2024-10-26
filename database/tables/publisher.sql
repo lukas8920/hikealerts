a80e775a37d85a6126583597b178f500ecd24e04
Lukas <lukas.kehrbusch@live.de>
2024-10-27 10:31:47 +1300

    added publisher.sql

---
+++ database/tables/publisher.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [hiking-sql-db].dbo.publisher (
+    id int IDENTITY(1,1) NOT NULL,
+    name varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    status varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
