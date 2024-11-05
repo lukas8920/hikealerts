e392d7019340db1a7d023d59794700bc13ac5af1
Lukas <lukas.kehrbusch@live.de>
2024-11-06 10:12:08 +1300

    adjusted service urls

--- angular/src/app/_service/user.service.ts
+++ angular/src/app/_service/user.service.ts
@@ -14,44 +14,46 @@ const textOptions = {
   providedIn: 'root'
 })
 export class UserService {
+  test_url: string = "http:localhost:8080";
+  prod_url: string = "https:hiking-alerts.org:8080"
 
   constructor(private http: HttpClient) { }
 
   register(mail: string, password: string): Observable<any> {
-    return this.http.post('http://localhost:8080/v1/auth/register', {
+    return this.http.post(this.prod_url + '/v1/auth/register', {
       mail, password
     }, httpOptions);
   }
 
   login(mail: string, password: string): Observable<any>{
-    return this.http.post('http://localhost:8080/v1/auth/login', {
+    return this.http.post(this.prod_url + '/v1/auth/login', {
       mail, password}, httpOptions);
   }
 
   resetPw(mail: string): Observable<any>{
-    return this.http.post('http://localhost:8080/v1/user/resetPassword', mail, textOptions);
+    return this.http.post(this.prod_url + '/v1/user/resetPassword', mail, textOptions);
   }
 
   changePw(newPassword: string, token: string | null): Observable<any> {
-    return this.http.post('http://localhost:8080/v1/user/changePassword', {
+    return this.http.post(this.prod_url + '/v1/user/changePassword', {
       newPassword, token
     }, httpOptions);
   }
 
   savePw(newPassword: string, token: string): Observable<any> {
-    return this.http.post('http://localhost:8080/v1/user/savePassword', {
+    return this.http.post( this.prod_url + '/v1/user/savePassword', {
       newPassword, token}, httpOptions);
   }
 
   refreshApiKey(): Observable<any> {
-    return this.http.post('http://localhost:8080/v1/user/refreshApiKey', httpOptions);
+    return this.http.post(this.prod_url + '/v1/user/refreshApiKey', httpOptions);
   }
 
   deleteAccount(): Observable<any>{
-    return this.http.post('http://localhost:8080/v1/user/deleteAccount', httpOptions);
+    return this.http.post( this.prod_url +'/v1/user/deleteAccount', httpOptions);
   }
 
   getProfile(): Observable<any>{
-    return this.http.get('http://localhost:8080/v1/user/profile', httpOptions);
+    return this.http.get(this.prod_url + '/v1/user/profile', httpOptions);
   }
 }
