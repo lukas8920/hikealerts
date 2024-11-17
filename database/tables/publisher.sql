24fcea0f59e70d82591ef6e7c66c6f622bd55ea0
Lukas <lukas.kehrbusch@live.de>
2024-11-18 10:38:05 +1300

    added license table fields

--- database/tables/publisher.sql
+++ database/tables/publisher.sql
@@ -1,5 +1,7 @@
 CREATE TABLE [hiking-sql-db].dbo.publisher (
     id int IDENTITY(1,1) NOT NULL,
     name varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-    status varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+    status varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    copyright varchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    license varchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 );
