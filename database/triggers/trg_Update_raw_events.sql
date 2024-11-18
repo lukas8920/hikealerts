a9b938c79c6cbd23be7886068afc8f570a5a9f24
Lukas <lukas.kehrbusch@live.de>
2024-11-18 19:16:31 +1300

    updated trigger to not consider ireland title changes

--- database/triggers/trg_Update_raw_events.sql
+++ database/triggers/trg_Update_raw_events.sql
@@ -1,9 +1,13 @@
 CREATE TRIGGER trg_Update_raw_events
-ON dbo.raw_events
-AFTER UPDATE
-AS
+    ON dbo.raw_events
+    AFTER UPDATE
+              AS
 BEGIN
-    INSERT INTO dbo.raw_events_CT (id, event_id, country, title, create_date, park_code, description, url, operation)
-    SELECT id, event_id, country, title, create_date, park_code, description, url, 'U'
-    FROM inserted;
+INSERT INTO dbo.raw_events_CT (id, event_id, country, title, create_date, park_code, description, url, operation)
+SELECT id, event_id, country, title, create_date, park_code, description, url, 'U'
+FROM inserted
+WHERE NOT (
+    UPDATE(title) AND
+    country IN ('IE')
+    );
 END;
