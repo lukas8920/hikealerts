22a4102b9c5fcb5e8c6509823346c8e2e5a9e19c
Lukas <lukas.kehrbusch@live.de>
2024-11-20 17:41:55 +1300

    enabled lazy loading of swagger css

--- angular/src/app/documentation/documentation.component.ts
+++ angular/src/app/documentation/documentation.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 import SwaggerUI from 'swagger-ui';
 
 @Component({
@@ -7,11 +7,21 @@ import SwaggerUI from 'swagger-ui';
   styleUrl: './documentation.component.css'
 })
 export class DocumentationComponent implements OnInit {
+  constructor(private renderer: Renderer2) {
+  }
+
   ngOnInit(): void {
+    this.loadSwaggerUICSS();
     SwaggerUI({
       url: '../../assets/openapi.yaml',
       domNode: document.getElementById('swagger-container')
     })
   }
 
+  private loadSwaggerUICSS(): void {
+    const link = this.renderer.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'assets/swagger-ui.css';
+    this.renderer.appendChild(document.head, link);
+  }
 }
