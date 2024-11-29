0e722f1b8a9af4318986009d11551bef2f154542
Lukas <lukas.kehrbusch@live.de>
2024-11-30 12:25:57 +1300

    added tile saving error logging

--- spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/repository/tiles/TileRepository.java
+++ spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/repository/tiles/TileRepository.java
@@ -2,6 +2,8 @@ package org.hikingdev.microsoft_hackathon.repository.tiles;
 
 import com.wdtinc.mapbox_vector_tile.VectorTile;
 import org.hikingdev.microsoft_hackathon.map_layer.Tile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import java.time.Duration;
 
 @Service
 public class TileRepository implements ITileRepository {
+    private static final Logger logger = LoggerFactory.getLogger(TileRepository.class);
     private static final byte[] emptyTile = VectorTile.Tile.newBuilder().build().toByteArray();
 
     private final RedisTemplate<String, byte[]> tileRepository;
@@ -21,8 +24,12 @@ public class TileRepository implements ITileRepository {
 
     @Override
     public void save(Tile tile) {
-        this.tileRepository.opsForHash().put(tile.getZoom(), tile.getTileKey(), tile.getTile());
-        this.tileRepository.expire(tile.getZoom(), Duration.ofHours(48));
+        try {
+            this.tileRepository.opsForHash().put(tile.getZoom(), tile.getTileKey(), tile.getTile());
+            this.tileRepository.expire(tile.getZoom(), Duration.ofHours(48));
+        } catch (Exception e){
+            logger.error("Error while saving.", e);
+        }
     }
 
     @Override
