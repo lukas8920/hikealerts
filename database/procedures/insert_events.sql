0ad7197a1e5292502ee3dbc5747c2b6d6f7a2d3b
Lukas <lukas.kehrbusch@live.de>
2025-02-02 12:32:24 +1300

    updated persistence method for events

--- database/procedures/insert_events.sql
+++ database/procedures/insert_events.sql
@@ -9,6 +9,8 @@ CREATE PROCEDURE InsertEvents
 	@mid_latitude_coordinate float,
 	@title varchar(MAX),
 	@description varchar(MAX),
+    -- trail ids
+	@ids NVARCHAR(MAX),
 	@publisher_id int,
 	@url varchar(MAX),
 	@override_data bit
@@ -45,6 +47,21 @@ BEGIN
             VALUES (source.event_id, source.region, source.country, source.create_date_time, source.from_date_time, source.to_date_time, source.mid_longitude_coordinate, source.mid_latitude_coordinate,
                     source.title, source.description, source.publisher_id, source.url)
     OUTPUT inserted.* INTO @OutputTable;
+
+    SET NOCOUNT ON;
+
+    DECLARE @TempTable TABLE (id BIGINT);
+
+    INSERT INTO @TempTable (id)
+    SELECT CAST(value AS BIGINT)FROM STRING_SPLIT(@ids, ',');
+
+    IF (SELECT count(id) FROM @TempTable t1 WHERE NOT EXISTS (SELECT 1 FROM events_trail_ids t2 WHERE t2.trail_ids = t1.id AND t2.event_id = @event_id)) > 0
+      OR (SELECT count(id) FROM @TempTable) <> (SELECT count(e.trail_ids) FROM events_trail_ids e WHERE e.event_id = @event_id)
+    BEGIN
+        DELETE FROM events_trail_ids WHERE event_id = @event_id;
+        INSERT INTO events_trail_ids (event_id, trail_ids)
+        SELECT @event_id, id FROM @TempTable;
+    END
 
     SELECT * FROM @OutputTable;
 END
