6a9e669e82ee5afe31cb181dfec84f0f90a4ee12
Lukas <lukas.kehrbusch@live.de>
2025-03-18 10:13:33 +1300

    fixed geotrek api endpoint

--- spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/geotrek/GeotrekDbService.java
+++ spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/geotrek/GeotrekDbService.java
@@ -2,9 +2,10 @@ package org.hikingdev.microsoft_hackathon.geotrek;
 
 import org.hikingdev.microsoft_hackathon.geotrek.entities.GeotrekUser;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface GeotrekDbService {
     @POST("/v1/geotrek/user")
-    Call<Void> postUser(GeotrekUser geotrekUser);
+    Call<Void> postUser(@Body GeotrekUser geotrekUser);
 }
