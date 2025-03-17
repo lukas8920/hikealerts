3f6098b1f8f5070a3651c00c18fad48d2e253d43
Lukas <lukas.kehrbusch@live.de>
2025-03-18 10:48:51 +1300

    changed id type for geotrek token to identity

--- spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/geotrek/entities/GeotrekToken.java
+++ spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/geotrek/entities/GeotrekToken.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 @Entity
 public class GeotrekToken {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private String userName;
