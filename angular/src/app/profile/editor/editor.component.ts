d8e268ec0d5902953f5f6de1c2a8e12f03ba15bd
Lukas <lukas.kehrbusch@live.de>
2025-03-19 22:36:50 +1300

    added window listeners for automatic login and loading bar

--- angular/src/app/profile/editor/editor.component.ts
+++ angular/src/app/profile/editor/editor.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {UserService} from '../../_service/user.service';
 import {TokenStorageService} from '../../_service/token-storage.service';
+import {SharedAppService} from '../../shared-app.service';
 
 @Component({
   selector: 'app-editor',
@@ -9,30 +10,66 @@ import {TokenStorageService} from '../../_service/token-storage.service';
   styleUrl: './editor.component.css'
 })
 export class EditorComponent implements AfterViewInit, OnDestroy {
+  initialUrl = "https://hiking-alerts.org:4200";
+  pathUrl = "http://hiking-alerts.org:4200/path/list/";
+
   safeUrl: SafeResourceUrl;
   isLoaded = false;
   isIframeVisible = false;
+  isLoading = false;
 
   constructor(private sanitizer: DomSanitizer, private userService: UserService,
-              private storage: TokenStorageService) {
-    const initialUrl = "https://hiking-alerts.org:4200"
-    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(initialUrl);
+              private storage: TokenStorageService, private sharedAppService: SharedAppService) {
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.initialUrl);
   }
 
-  ngAfterViewInit(): void {
+  private messageListener = (event: any) => {
+    console.log("received message");
+    const {type} = event.data
+
+    if (type == "LOGIN"){
+      this.requestLogin();
+    } else if (type == "LOADED"){
+      console.log("log in completed");
+      this.isLoading = false;
+      this.isIframeVisible = true;
+      this.sharedAppService.updateIsNavigating(false);
+    } else if (type == "RUNNING"){
+      console.log("active session is running");
+      this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.pathUrl);
+      this.isLoading = false;
+      this.isIframeVisible = true;
+    }
+  };
+
+  requestLogin(): void {
+    const iframe = document.getElementById('geotrek-iframe') as HTMLIFrameElement;
+    if (!this.isIframeVisible){
+      this.sharedAppService.updateIsNavigating(true);
+    }
     this.userService.getGeotrekToken().subscribe(o => {
       const iframe = document.getElementById('geotrek-iframe') as HTMLIFrameElement;
       iframe.onload = () => {
-        if(!this.isLoaded){
-          this.isLoaded = true;
-          iframe?.contentWindow?.postMessage({token: this.storage.getToken(), username: o.userName, password: o.password}, "*")
-          this.isIframeVisible = true;
-        }
+        iframe?.contentWindow?.postMessage({type: "LOGIN", token: this.storage.getToken(), username: o.userName, password: o.password}, "*")
       };
     })
   }
 
+  ngAfterViewInit(): void {
+    window.addEventListener('message', this.messageListener);
+    const iframe = document.getElementById('geotrek-iframe') as HTMLIFrameElement;
+    iframe.onload = () => {
+      console.log("send init request");
+      if (!this.isIframeVisible){
+        this.isLoading = true;
+        iframe?.contentWindow?.postMessage({type: "INIT"}, "*");
+      }
+    }
+  }
+
   ngOnDestroy(): void {
-    this.isLoaded = false;
+    this.isIframeVisible = false;
+    this.isLoading = false;
+    window.removeEventListener('message', this.messageListener);
   }
 }
