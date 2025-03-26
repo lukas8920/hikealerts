46610efee173a90cb3972f48413926c0aed90882
Lukas <lukas.kehrbusch@live.de>
2025-03-26 23:59:40 +1300

    adjusted method to identify trails in France

--- spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/event_handling/event_injection/countries/FRInjector.java
+++ spring-boot/src/main/java/org/hikingdev/microsoft_hackathon/event_handling/event_injection/countries/FRInjector.java
@@ -6,7 +6,9 @@ import org.hikingdev.microsoft_hackathon.repository.raw_events.IRawEventReposito
 import org.hikingdev.microsoft_hackathon.repository.regions.IRegionRepository;
 import org.hikingdev.microsoft_hackathon.repository.trails.ITrailRepository;
 import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.io.ParseException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FRInjector extends RegionInjector {
@@ -22,5 +24,18 @@ public class FRInjector extends RegionInjector {
     @Override
     protected List<Region> findRegionsInDatabase(String regionName, String country) {
         return this.iRegionRepository.findUniqueRegionName(regionName, country);
+    }
+
+    @Override
+    protected List<Event> identifyTrail(RawEvent rawEvent, Event event, OpenAiEvent openAiEvent) throws ParseException {
+        List<Event> events = new ArrayList<>();
+        //find best matching trail
+        Trail trail = this.iTrailRepository.searchTrailByNameAndCountry(openAiEvent.getTrailName(), event.getCountry(), this.provideNameMatcher());
+        if (trail != null){
+            event.setTrailIds(List.of(trail.getId()));
+            event.calculateMidCoordinate(trail);
+            events.add(event);
+        }
+        return events;
     }
 }
